670667ce036afdd22363020a835e1d66ae864503
nayurematias <nayurematias@gmail.com>
2021-12-15 23:46:23 -0300

    consertos

---
+++ consertos.js
@@ -0,0 +1,33 @@
+const consertos = [
+    {
+        aparelho: 'Samsung A10',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            10,
+            100,
+            25
+        ],
+        pronto: true
+    },
+
+    {
+        aparelho: 'Samsung A20',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            10,
+            100,
+            25
+        ],
+        pronto: false
+    }
+]
+
+module.exports = consertos;
